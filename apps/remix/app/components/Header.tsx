c398276a102cf6ac0ff08e3fa8dd426c6de2e2c0
iougemini <163802753+iougemini@users.noreply.github.com>
2024-06-05 12:51:43 +0800

    Update Header.tsx

--- apps/remix/app/components/Header.tsx
+++ apps/remix/app/components/Header.tsx
@@ -10,15 +10,15 @@ export default function Header() {
       <Link to="/" className="font-bold flex items-center justify-center gap-3">
         <VmailLogo />
         <button className="cool-btn">
-          <span>VMAIL.DEV</span>
+          <span>NIE.MAIL</span>
         </button>
       </Link>
-      <Link
+      <!--<Link
         to="https://chat.sorapi.dev"
         target="_blank"
         className="ml-auto text-sm md:text-base hidden md:block">
         {t("GPT")}
-      </Link>
+      </Link>-->
       <Link
         to="/about"
         className="ml-3 md:ml-8 text-sm md:text-base hidden md:block">
